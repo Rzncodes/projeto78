b25cb2802ac31ba79733da4a69c822fa4371b064
Rzncodes <125281324+Rzncodes@users.noreply.github.com>
2023-12-13 11:06:53 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+var firebaseConfig = {
+  //Cole sua Configuração do Firebase aqui
+  
+    apiKey: "AIzaSyCPoJTmBfQWsSmpianI3SBaEW-S-Xwixxc",
+    authDomain: "projeto78-55102.firebaseapp.com",
+    projectId: "projeto78-55102",
+    storageBucket: "projeto78-55102.appspot.com",
+    messagingSenderId: "75358300282",
+    appId: "1:75358300282:web:d0996638a488fe4d29f3c3"
+  
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
